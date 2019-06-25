f1622afb2d8c394e955e4987773e5548908e139b
fengyang <568931300@qq.com>
2019-06-25 17:03:22 +0800

    processing

---
+++ ftp/Modular/processing.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Time    :2019/6/25 16:57
+'''
+文件处理
+'''
+
+
+def processing_file(put_path, down_path):
+    with open(put_path) as f1, open(down_path) as f2:
+        pass
